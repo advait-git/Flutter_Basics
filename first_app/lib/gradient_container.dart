0e41336e60d5f15b19e50324e608297678879098
advait-git <advaittiwari2@gmail.com>
2024-08-08 19:07:55 +0530

    dice roller app

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:first_app/text.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAllignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+   const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAllignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
---
+++ first_app/lib/text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextFormat extends StatelessWidget{
+  const TextFormat(this.text,{super.key});
+
+  final String text; 
+
+  @override
+  Widget build(context) {
+    return Text(
+              text,
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 244, 238, 238
+                  ), 
+                  fontSize: 35
+                  ),
+            );
+  }
+}
